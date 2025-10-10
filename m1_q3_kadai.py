c7e12ceeb930672c6d3291ecede38a2c582d81ff
itc-n24009 <n24009@std.it-college.ac.jp>
2025-10-10 01:57:28 +0000

    1010 kadai

---
+++ m1_q3_kadai.py
@@ -0,0 +1,10 @@
+a = 12
+b = 5
+print(abs(b - a))
+print(b - abs(2 * b - a) * 3)
+print("{0:0b}".format(64))
+c = [2, 7, 15, 12, 9]
+c.sort(reverse=True)
+print(c)
+print(c[1:4])
+print(f'{(a - b) / c[3]:_<8.1f}')
