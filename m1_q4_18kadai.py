d039bc7ab6cb102d70f082b0eeee7409c6fd5021
itc-n24009 <n24009@std.it-college.ac.jp>
2025-10-10 03:01:54 +0000

    1010 kadai

--- m1_q4_18kadai.py
+++ m1_q4_18kadai.py
@@ -5,5 +5,6 @@ for p in phrase:
         list_p.append(p)
 print(len(phrase) - len(list_p))
 
-for i in list_p:
-    print(i,end="")
+for i in list_p: #ループ処理で一文字ずつリストから取る
+    print(i,end="") #改行はしたくないのでend=""で改行しないで出力する
+print() #改行の出力
