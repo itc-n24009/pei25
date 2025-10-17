af50cdcdcc13c08bc5e6b3998eb6524d490490dd
itc-n24009 <n24009@std.it-college.ac.jp>
2025-10-17 02:16:10 +0000

    1017 kadai

---
+++ m1_q4_20.py
@@ -0,0 +1,7 @@
+a = 0b11010
+b = 0b10100
+c = a & b
+d = a | b
+c >>= 2
+e = d - c
+print(f'{e:b}')
---
+++ m1_q4_21kadai.py
@@ -0,0 +1,12 @@
+num = 12
+list_s = []
+s = 2
+while s <= num:
+    if num % s == 0:
+        num //= s
+        if s not in list_s:
+            list_s.append(s)
+    else:
+        s += 1
+    print(num)
+print(list_s)
